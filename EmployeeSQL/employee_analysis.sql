a26d78b1693e7ccf8ada991ec84aa1985bedd2bf
Skinsella <kinsella.sj@gmail.com>
2019-10-15 18:21:48 -0500

    queries finished

--- EmployeeSQL/employee_analysis.sql
+++ EmployeeSQL/employee_analysis.sql
@@ -16,10 +16,20 @@ WHERE Hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 --3. List the manager of each department with the following information: 
 --department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
-SELECT dept_manager.Dept_no, departments.Dept_name
+SELECT dept_manager.Dept_no, departments.Dept_name, dept_manager.Emp_no, employees.First_name, employees.Last_name, dept_manager.From_date, dept_manager.To_date
+FROM dept_manager
+LEFT JOIN departments
+ON dept_manager.Dept_no = departments.Dept_no
+LEFT JOIN employees
+ON dept_manager.Emp_no = employees.Emp_no;
 
 --4. List the department of each employee with the following information: employee number, last name, first name, and department name.
-
+SELECT departments.Dept_name, dept_emp.Emp_no, employees.Last_name, employees.First_name
+FROM dept_emp
+LEFT JOIN departments
+ON dept_emp.Dept_no = departments.Dept_no
+LEFT JOIN employees
+ON dept_emp.Emp_no = employees.Emp_no;
 
 --5. List all employees whose first name is "Hercules" and last names begin with "B."
 SELECT First_name, Last_name
@@ -27,11 +37,22 @@ FROM employees
 WHERE First_name = 'Hercules' and Last_name LIKE 'B%'
 
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
-
+SELECT departments.Dept_name, dept_emp.Emp_no, employees.Last_name, employees.First_name
+FROM dept_emp
+LEFT JOIN departments
+ON dept_emp.Dept_no = departments.Dept_no
+LEFT JOIN employees
+ON dept_emp.Emp_no = employees.Emp_no
+WHERE departments.Dept_name = 'Sales';
 
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+SELECT departments.Dept_name, dept_emp.Emp_no, employees.Last_name, employees.First_name
+FROM dept_emp
+LEFT JOIN departments
+ON dept_emp.Dept_no = departments.Dept_no
+LEFT JOIN employees
+ON dept_emp.Emp_no = employees.Emp_no
+WHERE departments.Dept_name = 'Sales' OR departments.Dept_name = 'Development';
 
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
